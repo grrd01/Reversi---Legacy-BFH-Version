7d4c86ddc8621e035212f4014cfd93929de349c1
guyen <guyen@gmx.ch>
2015-09-10 09:36:56 +0200

    bugfix gameLogic

--- source/client/js/gameLogic.js
+++ source/client/js/gameLogic.js
@@ -94,7 +94,7 @@ var gameLogic = function() {
             var possibleMoves = [];
             for (var i = 0; i < 8; i++) {
                 for (var j = 0; j < 8; j++) {
-                    if (isPossibleMove(state,i,j)) {
+                    if (board[i][j] == 0 && isPossibleMove(state,i,j)) {
                         possibleMoves.push([i, j]);
                     }
                 }
